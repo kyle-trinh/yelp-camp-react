f076b8aad6727e450edf5f329a350ba2476f2f0e
Binh Trinh <trinhthaibinh.ecom@gmail.com>
2019-06-20 10:22:26 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const path = require('path');
+
+// Initialize app var with express
+
+const app = express();
+
+// Connect Database
+
+connectDB();
+
+// Init Middleware
+
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => res.send('API running'));
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
