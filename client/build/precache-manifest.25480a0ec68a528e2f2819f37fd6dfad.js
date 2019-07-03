7c174f7fd25cf366c196c13db83b1d5704e6acc4
Binh Trinh <trinhthaibinh.ecom@gmail.com>
2019-07-03 06:13:28 -0700

    Fix cover photo for mobile

---
+++ client/build/precache-manifest.25480a0ec68a528e2f2819f37fd6dfad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd542f3276c1049689ced34c62d2bc44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af08415f521e47004464",
+    "url": "/static/css/main.dfd3bcd5.chunk.css"
+  },
+  {
+    "revision": "a2abd48c84251a114b0a",
+    "url": "/static/js/2.b1668e72.chunk.js"
+  },
+  {
+    "revision": "af08415f521e47004464",
+    "url": "/static/js/main.48a32513.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
